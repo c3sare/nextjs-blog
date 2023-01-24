1baf48e2ac57a81fef55c8c3ca76ff0122699a35
C3sare <marcinm222@gmail.com>
2023-01-24 15:32:48 +0100

    date in polish

--- components/Date.js
+++ components/Date.js
@@ -1,6 +1,8 @@
-import { parseISO, format } from 'date-fns';
+import { parseISO, format, setDefaultOptions } from 'date-fns';
+import { pl } from "date-fns/locale";
 
 export default function Date({ dateString }) {
+  setDefaultOptions({ locale: pl });
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
